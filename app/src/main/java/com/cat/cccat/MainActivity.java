fc399f4d7aca9e972b706d5dbb12405b1d0d093d
tianwei <657343741@qq.com>
2017-11-18 11:19:11 +0800

    cccat

---
+++ app/src/main/java/com/cat/cccat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cat.cccat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
